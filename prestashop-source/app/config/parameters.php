5ffc8dea0b92d2146c19aa703f5fcfa208e35fa5
Mike <klim.kolesnev@gmail.com>
2026-01-25 19:07:31 +0100

    change for Dockerfile, parameters.php and .htaccess

--- prestashop-source/app/config/parameters.php
+++ prestashop-source/app/config/parameters.php
@@ -1,11 +1,11 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => 'mariadb',
+    'database_host' => 'admin-mysql_db',
     'database_port' => '',
-    'database_name' => 'prestashop',
-    'database_user' => 'admin',
-    'database_password' => 'admin',
+    'database_name' => 'BE_196638',
+    'database_user' => 'root',
+    'database_password' => 'student',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
